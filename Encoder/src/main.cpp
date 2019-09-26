41c017036d0c5e4f2c3d238770fe3dee470ff07c
Tee-Hunter <theechate@gmail.com>
2019-09-26 17:44:09 +0900

    First commit for Encoding G-DORM Lab experiment. The first version code is for basic using Rotary Encoder: 1. Pulse comtrolling number. 2. Determining positive and Negative pulse value.

---
+++ Encoder/src/main.cpp
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+
+volatile signed int temp, counter = 0; //This variable will increase or decrease depending on the rotation of encoder
+
+//Calling Function  
+void ai0();
+void ai1();
+    
+void setup() {
+  Serial.begin (9600);
+
+    pinMode(2, INPUT_PULLUP); // internal pullup input pin 2 
+    
+    pinMode(3, INPUT_PULLUP); // internal pullup input pin 3
+    //Setting up interrupt
+    //A rising pulse from encodenren activated ai0(). DigitalPin 2
+    attachInterrupt(digitalPinToInterrupt(2), ai0, RISING);
+    
+    //B rising pulse from encodenren activated ai1(). DigitalPin 3
+    attachInterrupt(digitalPinToInterrupt(3), ai1, RISING);
+  }
+   
+  void loop() {
+      // Send the value of counter
+      if( counter != temp ){
+      Serial.println (counter);
+      temp = counter;
+    }
+  }
+   
+  void ai0() {
+    // ai0 is activated if DigitalPin 2 is going from LOW to HIGH
+    // Check pin 3 to determine the direction
+    if(digitalRead(3) == LOW) {
+      counter++;
+    }
+      else{
+        counter--;
+    }
+  }
+   
+  void ai1() {
+    // ai0 is activated if DigitalPin 3 is going from LOW to HIGH
+    // Check with pin 2 to determine the direction
+    if(digitalRead(2) == LOW) {
+      counter--;
+    }
+    else{
+      counter++;
+    }
+  }
+
+
