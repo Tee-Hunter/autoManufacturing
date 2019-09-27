1039cb1641c5820e266cef419c98bd12995051db
Tee-Hunter <theechate@gmail.com>
2019-09-27 13:23:30 +0900

    #calculate rotation angle

--- Encoder/src/main.cpp
+++ Encoder/src/main.cpp
@@ -1,12 +1,13 @@
 // Rotary encoder basic program and circuit
-
 #include <Arduino.h>
 
 volatile signed int temp, counter = 0; //This variable will increase or decrease depending on the rotation of encoder
+float angle = 0.0; // define the angle variable
 
 //Calling Function  
 void ai0();
 void ai1();
+void rotationAngle (int PPR);
     
 void setup() {
   Serial.begin (9600);
@@ -15,41 +16,52 @@ void setup() {
     
     pinMode(3, INPUT_PULLUP); // internal pullup input pin 3
     //Setting up interrupt
-    //A rising pulse from encodenren activated ai0(). DigitalPin 2
+    //A rising pulse from encoders activated ai0(). DigitalPin 2
     attachInterrupt(digitalPinToInterrupt(2), ai0, RISING);
     
-    //B rising pulse from encodenren activated ai1(). DigitalPin 3
+    //B rising pulse from encoders activated ai1(). DigitalPin 3
     attachInterrupt(digitalPinToInterrupt(3), ai1, RISING);
   }
    
-  void loop() {
-      // Send the value of counter
-      if( counter != temp ){
-      Serial.println (counter);
+void loop() {
+  // Send the value of counter
+  if( counter != temp ){
+      Serial.print("Pulse : ");
+      Serial.println(counter);
       temp = counter;
-    }
+      rotationAngle (counter);
   }
+  // delay(1000);
+}
    
-  void ai0() {
-    // ai0 is activated if DigitalPin 2 is going from LOW to HIGH
-    // Check pin 3 to determine the direction
-    if(digitalRead(3) == LOW) {
+void ai0() {
+  // ai0 is activated if DigitalPin 2 is going from LOW to HIGH
+  // Check pin 3 to determine the direction
+  if(digitalRead(3) == LOW) {
       counter++;
-    }
-      else{
-        counter--;
-    }
   }
-   
-  void ai1() {
-    // ai0 is activated if DigitalPin 3 is going from LOW to HIGH
-    // Check with pin 2 to determine the direction
-    if(digitalRead(2) == LOW) {
+  else{
       counter--;
-    }
-    else{
-      counter++;
-    }
   }
+}
+   
+void ai1() {
+  // ai0 is activated if DigitalPin 3 is going from LOW to HIGH
+  // Check with pin 2 to determine the direction
+  if(digitalRead(2) == LOW) {
+    counter--;
+  }
+  else{
+    counter++;
+  }
+}
 
-
+void rotationAngle (int PPR) {
+  // define the angle variable as global
+  // calculate DPP form counter parameter
+  angle = 360.0 / PPR;
+  // print out angle (DPP)
+  Serial.print("Angle : ");
+  Serial.print(angle);
+  Serial.println("°");
+}
