fc59cfbb3d75205043c49f73f273b8a3053cde20
Tee-Hunter <theechate@gmail.com>
2019-09-27 14:27:58 +0900

    rotation speed and update rotation angle

--- Encoder/src/main.cpp
+++ Encoder/src/main.cpp
@@ -2,12 +2,15 @@
 #include <Arduino.h>
 
 volatile signed int temp, counter = 0; //This variable will increase or decrease depending on the rotation of encoder
-float angle = 0.0; // define the angle variable
+float Angle = 0.0; // define the angle variable
+int Speed = 0;  // declare the speed variable
+int resolution = 1000;
 
 //Calling Function  
 void ai0();
 void ai1();
 void rotationAngle (int PPR);
+void rotatoinSpeed (int NoP);
     
 void setup() {
   Serial.begin (9600);
@@ -30,6 +33,7 @@ void loop() {
       Serial.println(counter);
       temp = counter;
       rotationAngle (counter);
+      rotatoinSpeed (counter);
   }
   // delay(1000);
 }
@@ -56,12 +60,22 @@ void ai1() {
   }
 }
 
-void rotationAngle (int PPR) {
-  // define the angle variable as global
-  // calculate DPP form counter parameter
-  angle = 360.0 / PPR;
+void rotationAngle (int NoP) {
+  // decare the angle variable as global
+  // calculate DPP form Encoder value
+  Angle = (NoP * 360.0) / resolution;
   // print out angle (DPP)
   Serial.print("Angle : ");
-  Serial.print(angle);
+  Serial.print(Angle);
   Serial.println("°");
+}
+
+void rotatoinSpeed (int NoP) {
+  // declare the speed variable as global
+  // calculate Speed form Encoder value
+  Speed = (NoP * 60) / resolution;
+  // Print out the Speed
+  Serial.print("Speed : ");
+  Serial.print(Speed);
+  Serial.println(" RPM");
 }
