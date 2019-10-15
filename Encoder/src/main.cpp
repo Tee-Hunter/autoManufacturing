5df8dd4612601d9adde3f82da90d58d07ee23c16
Tee-Hunter <theechate@gmail.com>
2019-10-15 12:23:38 +0900

    comment angle out cuz i dont nedd to use it in purpose

--- Encoder/src/main.cpp
+++ Encoder/src/main.cpp
@@ -1,79 +1,102 @@
 // Rotary encoder basic program and circuit
 #include <Arduino.h>
 
-volatile int temp, counter = 0; //This variable will increase or decrease depending on the rotation of encoder
-unsigned int angle = 0;
-int resolution = 1000;
+int encoderPin1 = 2; // Black
+int encoderPin2 = 3; // White
 
-//Calling Function  
-void outA();
-void outB();
-// void angular(unsigned int PPR);
+// motor
+int BIN1 = 8;      //PIN 21
+int BIN2 = 7;      //PIN 22
+int PWMB = A0;    //PIN 23
+int STBY = 9;   // standby
+
+volatile int lastEncoded = 0;
+volatile long encoderValue = 0;
+
+// int angle = 0;
+int angularSpeed = 0;
+
+long lastencoderValue = 0;
+
+int lastMSB = 0;
+int lastLSB = 0;
+
+void updateEncoder();
 
 void setup() {
   Serial.begin (9600);
 
-    pinMode(2, INPUT_PULLUP); // internal pullup input pin 2 
-    pinMode(3, INPUT_PULLUP); // internal pullup input pin 3
-    
+  pinMode(encoderPin1, INPUT_PULLUP); 
+  pinMode(encoderPin2, INPUT_PULLUP);
 
-    //Setting up interrupt
-    //A rising pulse from encoders activated outA(). DigitalPin 2
-    attachInterrupt(digitalPinToInterrupt(2), outA, RISING);
-    
-    //B rising pulse from encoders activated outB(). DigitalPin 3
-    attachInterrupt(digitalPinToInterrupt(3), outB, RISING);
-  }
-   
-void loop() {
-  // Send the value of counter
-  if ( counter != temp ){
-      Serial.print("counter \t: ");
-      Serial.println(counter);
-      
-      // angular(counter);
-      angle = (counter*360)/1000;
-      Serial.print("Angle \t\t: ");
-      Serial.println(angle);
-  }
-  // check status
-  temp = counter;
-}
-   
-void outA() {
-  // outA is activated if DigitalPin 2 is going from LOW to HIGH
-  // Check pin 3 to determine the direction
-  if (digitalRead(3) == LOW) {
-      counter++;
-      if (counter > 1000)
-        counter = 1000;
-  }
-  else {
-      counter--;
-      if (counter < 0)
-        counter = 1000;
-  }
-}
-   
-void outB() {
-  // outA is activated if DigitalPin 3 is going from LOW to HIGH
-  // Check with pin 2 to determine the direction
-  if (digitalRead(2) == LOW) {
-    counter--;
-    if (counter < 0)
-        counter = 0;
-  }
-  else {
-    counter++;
-    if (counter > 1000)
-        counter = 1000;
-  }
+  //motor output define
+  pinMode(BIN1, OUTPUT); 
+  pinMode(BIN2, OUTPUT); 
+  pinMode(PWMB, OUTPUT);
+  pinMode(STBY, OUTPUT);
+
+  digitalWrite(encoderPin1, HIGH); //turn pullup resistor on
+  digitalWrite(encoderPin2, HIGH); //turn pullup resistor on
+
+  //call updateEncoder() when any high/low changed seen
+  //on interrupt 0 (pin 2), or interrupt 1 (pin 3) 
+  attachInterrupt(0, updateEncoder, CHANGE); 
+  attachInterrupt(1, updateEncoder, CHANGE);
+
 }
 
-// void angular(unsigned int PPR) {
-//   angle = (counter*360)/1000;
-//   Serial.print("Angle : ");
-//   Serial.println(angle);
-// }
+void loop(){ 
+  //Do stuff here
+  if (lastencoderValue != encoderValue) {
+    //pulse
+    Serial.print("counter: ");
+    Serial.print(encoderValue);
+    Serial.print("\t\t | \t\t");
+
+    //angle
+    // angle = encoderValue*360 / 1000;
+
+    // if (angle > 360) angle = 360;
+    // if (angle <= 0) angle = 0;
+
+    // Serial.print("angle: \t");
+    // Serial.print(angle);
+    // Serial.print("\t");
+
+    // angular speed (rad/s)
+    angularSpeed = 2*PI*encoderValue / 1000;
+
+    if (angularSpeed > 255) angularSpeed = 255;
+    if (angularSpeed <= 0) angularSpeed = 0;
+
+    Serial.print("speed: \t");
+    Serial.println(angularSpeed);
+  }
+  lastencoderValue = encoderValue;
+  
+  // motor control
+  digitalWrite(STBY, HIGH);
+
+  digitalWrite(BIN1, HIGH);
+  digitalWrite(BIN2, LOW);
+  analogWrite(PWMB, angularSpeed);
+  //Serial.println(angularSpeed);
+
+}
 
 
+void updateEncoder(){
+  int MSB = digitalRead(encoderPin1); //MSB = most significant bit
+  int LSB = digitalRead(encoderPin2); //LSB = least significant bit
+
+  int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number
+  int sum  = (lastEncoded << 2) | encoded; //adding it to the previous encoded value
+
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderValue ++;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue --;
+
+  if (encoderValue > 40600) encoderValue = 40600;
+  if (encoderValue <= 0) encoderValue = 0;
+
+  lastEncoded = encoded; //store this value for next time
+}
